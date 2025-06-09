68976b4105a0fd6ff1ce08ad6cd8e0c23fe14fd4
rohit-p-droid <76893151+rohit-p-droid@users.noreply.github.com>
2025-06-09 23:19:36 +0530

    init: project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-center">
+      <h1>Smart Resume Analyzer & Job Matcher</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
